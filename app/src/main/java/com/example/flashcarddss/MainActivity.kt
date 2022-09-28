2154b22dfa756f078429afc971fac8c2f6250995
Kevin <87961130+Dridgea@users.noreply.github.com>
2022-09-28 19:59:27 -0400

    Added Ability to add new Flashcards.

---
+++ app/src/main/java/com/example/flashcarddss/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.flashcarddss
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (data != null) { // Check that we have data returned
+                val string1 = data.getStringExtra("QUESTIONKEY") // 'string1' needs to match the key we used when we put the string in the Intent
+                val string2 = data.getStringExtra("ANSWERKEY")
+                // Log the value of the strings for easier debugging
+                Log.i("MainActivity", "Question: $string1")
+                Log.i("MainActivity", "Answer: $string2")
+            } else {
+                Log.i("MainActivity", "Returned null data from AddCardActivity")
+            }
+        }
+
+        findViewById<View>(R.id.myBtn).setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            // Launch EndingActivity with the resultLauncher so we can execute more code
+            // once we come back here from EndingActivity
+            resultLauncher.launch(intent)
+        }
+
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.GONE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.GONE
+        }
+    }
+}
